1b85bf7551601ab12453897807ca2d1acb99669c
Tommaso Di Vito <tommaso.divito2@gmail.com>
2022-01-14 16:59:08 +0100

    Delete Jlambda/src/main/java/com/jlambda/lang directory

--- Jlambda/src/main/java/com/jlambda/lang/JLBuiltin.java
+++
@@ -1,8 +0,0 @@
-package com.jlambda.lang;
-
-public class JLBuiltin {
-
-    public static boolean jlEquals(Object o1, Object o2){
-            return o1.equals(o2);
-    }
-}
--- Jlambda/src/main/java/com/jlambda/lang/example.jl
+++
@@ -1,3 +0,0 @@
-let if = fun c -> fun then -> fun else -> (c(then)(else))
-let Y = fun f -> (fun x -> (f (x(x)))(fun x -> (f (x(x)))))
-let one = 1 in ==(one)(one) # assuming == is native defined #
